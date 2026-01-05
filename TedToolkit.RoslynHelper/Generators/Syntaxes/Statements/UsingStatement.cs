c96ee4478fc3daeadead3d6621ad0d8fe506ffc1
秋水 <1123993881@qq.com>
2026-01-05 17:03:09 +0800

    ✨ feat(roslyn-gen): Implement support for C# using statement generation
    This commit introduces the `UsingStatement` class, enabling the generation of C# `using` statements (resource statements). This structure ensures proper resource disposal for disposable objects, allowing generated code to correctly scope and manage resources within a defined block.
    
    *   Implement the `UsingStatement` class, supporting `using (Type identifier = expression) { ... }` syntax generation.
    *   The new class implements `IStatement`, `IVariable`, and `IStatementOwner`.
    *   Update the `TedToolkit` external submodule reference.

---
+++ TedToolkit.RoslynHelper/Generators/Syntaxes/Statements/UsingStatement.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UsingStatement.cs" company="TedToolkit">
+// Copyright (c) TedToolkit. All rights reserved.
+// Licensed under the LGPL-3.0 license. See COPYING, COPYING.LESSER file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Cysharp.Text;
+
+namespace TedToolkit.RoslynHelper.Generators.Syntaxes;
+
+/// <summary>
+/// the using statement
+/// </summary>
+/// <param name="type">type</param>
+/// <param name="identifier">identifier</param>
+/// <param name="expression">expression</param>
+public sealed class UsingStatement(DataType type, string identifier, IExpression expression) :
+    IStatement,
+    IVariable,
+    IStatementOwner
+{
+    private DataType _type = type;
+
+    /// <inheritdoc />
+    public void ToCode(ref SourceBuilder builder)
+    {
+        builder.Append("using (");
+        _type.ToCode(ref builder);
+        builder.Append(" @");
+        builder.Append(identifier);
+        builder.Append(" = ");
+        expression.ToCode(ref builder);
+        builder.Append(')');
+        this.AddStatements(ref builder);
+    }
+
+    /// <inheritdoc/>
+    public string Variable
+        => ZString.Concat('@', identifier);
+
+    /// <inheritdoc />
+    public List<IStatement> Statements
+        => field ??= [];
+}
