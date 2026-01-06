c047d837c08ff9a52358dc0efdeea1c50caed5f5
秋水 <1123993881@qq.com>
2026-01-06 15:21:48 +0800

    🔧 chore: Correct file reference in copyright header
    This commit fixes an inconsistency in the C# file's copyright header by correcting the referenced file name. The documentation header incorrectly named the file, leading to potential confusion regarding file metadata.
    
    * Update the `file` attribute in the copyright header from `ArgumentExtensions.cs` to the correct file name, `ArgumentReferenceExtensions.cs`.

--- TedToolkit.RoslynHelper/Generators/Extensions/ArgumentReferenceExtensions.cs
+++ TedToolkit.RoslynHelper/Generators/Extensions/ArgumentReferenceExtensions.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="ArgumentExtensions.cs" company="TedToolkit">
+// <copyright file="ArgumentReferenceExtensions.cs" company="TedToolkit">
 // Copyright (c) TedToolkit. All rights reserved.
 // Licensed under the LGPL-3.0 license. See COPYING, COPYING.LESSER file in the project root for full license information.
 // </copyright>
