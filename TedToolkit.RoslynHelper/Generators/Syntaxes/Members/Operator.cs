adbfd3ff4b7aef3a19cce37eba98b635ed575ac4
秋水 <1123993881@qq.com>
2026-01-07 14:23:38 +0800

    🎨 style: Add copyright and license header to Operator.cs
    This change standardizes the file header for `Operator.cs` by introducing the required copyright notice and LGPL-3.0 license reference. Ensuring consistent file headers helps maintain legal compliance and project stylistic requirements across the repository.
    
    * Added standard header block defining copyright information for TedToolkit.
    * Included license reference (LGPL-3.0) in the header comments.

--- TedToolkit.RoslynHelper/Generators/Syntaxes/Members/Operator.cs
+++ TedToolkit.RoslynHelper/Generators/Syntaxes/Members/Operator.cs
@@ -1,4 +1,11 @@
-﻿namespace TedToolkit.RoslynHelper.Generators.Syntaxes;
+﻿// -----------------------------------------------------------------------
+// <copyright file="Operator.cs" company="TedToolkit">
+// Copyright (c) TedToolkit. All rights reserved.
+// Licensed under the LGPL-3.0 license. See COPYING, COPYING.LESSER file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TedToolkit.RoslynHelper.Generators.Syntaxes;
 
 /// <summary>
 /// Add the operator
