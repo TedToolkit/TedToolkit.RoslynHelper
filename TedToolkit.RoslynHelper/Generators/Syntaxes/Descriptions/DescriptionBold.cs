a9739e00cf14b06b6807d85281abef4dde2cc769
秋水 <1123993881@qq.com>
2026-06-05 15:23:22 +0800

    feat: add bold and italic.

---
+++ TedToolkit.RoslynHelper/Generators/Syntaxes/Descriptions/DescriptionBold.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DescriptionBold.cs" company="TedToolkit">
+// Copyright (c) TedToolkit. All rights reserved.
+// Licensed under the LGPL-3.0 license. See COPYING, COPYING.LESSER file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TedToolkit.RoslynHelper.Generators.Syntaxes;
+
+/// <summary>
+/// Bold.
+/// </summary>
+/// <param name="descriptions">Descriptions.</param>
+public sealed class DescriptionBold(params IReadOnlyList<IDescriptionItem> descriptions) : IDescriptionItem
+{
+    /// <inheritdoc />
+    public void ToDescription(ref SourceBuilder builder)
+    {
+        if (descriptions.Count == 0)
+        {
+            return;
+        }
+
+        builder.AppendLine("/// <b>");
+        descriptions.ToDescription(ref builder);
+        builder.AppendLine("/// </b>");
+    }
+}
---
+++ TedToolkit.RoslynHelper/Generators/Syntaxes/Descriptions/DescriptionItalic.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DescriptionItalic.cs" company="TedToolkit">
+// Copyright (c) TedToolkit. All rights reserved.
+// Licensed under the LGPL-3.0 license. See COPYING, COPYING.LESSER file in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TedToolkit.RoslynHelper.Generators.Syntaxes;
+
+/// <summary>
+/// Italic.
+/// </summary>
+/// <param name="descriptions">Descriptions.</param>
+public sealed class DescriptionItalic(params IReadOnlyList<IDescriptionItem> descriptions) : IDescriptionItem
+{
+    /// <inheritdoc />
+    public void ToDescription(ref SourceBuilder builder)
+    {
+        if (descriptions.Count == 0)
+        {
+            return;
+        }
+
+        builder.AppendLine("/// <i>");
+        descriptions.ToDescription(ref builder);
+        builder.AppendLine("/// </i>");
+    }
+}
